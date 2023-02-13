eed0b886b33d2eee7d42102c3ce93c3347d2b7fd
Jonathan74 <jonathan.engles74@gmail.com>
2023-02-13 15:36:30 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# scroll-modale_mini-projet
+# Mini projet d'une Modale Au Scroll
+
+- quand on scrolle de 1000px vers le bas, une modale s'ouvre.
+- au clique sur le bouton, un lien vers mon profil linkedIn apparait
+- au clique sur le lien, ma page LinkedIn s'ouvre.
+- au clique sur la croix, la modale disparait et l'event du scroll est annulé.
+
+
+[Modale Au Scroll](https://panzer-deserter.000webhostapp.com/)
---
+++ app.js
@@ -0,0 +1,42 @@
+console.log('Hello World');
+//on sélectionne et on stocke la modal 
+const modal = document.querySelector('.modal-container');
+
+//on sélectionne et on stocke le bouton 
+const btn = document.querySelector('button');
+
+const profil = document.querySelector('.profil-linkedin');
+// console.log(profil);
+
+const icone = document.querySelector('.fa-times');
+// console.log(icone);
+
+function displayModale() {
+  let hauteur = document.documentElement.scrollTop;  
+  //   console.log(hauteur + 'nb de pixels parcouru depuis le haut');
+  if ( hauteur > 1000) {
+    console.log('affichage de la modale!!');
+    modal.style.display = 'flex';
+  }
+}
+
+window.addEventListener('scroll', displayModale);
+
+btn.addEventListener('click', () => {
+
+  //au click, je fais apparaitre le profil...
+  profil.style.display = 'block';
+
+  //...et disparaitre le bouton
+  btn.style.display = 'none';
+});
+
+
+icone.addEventListener('click', () => {
+  //au click, je fais disparaitre la modale
+  modal.style.display = 'none';
+
+  // j'annule l'évènement du scroll ppur ne pas reproduire l'apparation de la modale
+
+  window.removeEventListener('scroll', displayModale);
+});
